475fcf7b781c7826139678903c56c59f9a419cc2
Kostiantyn Logunov <777koc@gmail.com>
2021-05-12 00:31:50 +0300

    Entity notification was added

--- src/Logunov.Facts/Logunov.Facts.Web/Data/Migrations/20210511213056_EntityNotificationAdded.Designer.cs
+++ src/Logunov.Facts/Logunov.Facts.Web/Data/Migrations/20210511213056_EntityNotificationAdded.Designer.cs
@@ -4,14 +4,16 @@ using Logunov.Facts.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Logunov.Facts.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210511213056_EntityNotificationAdded")]
+    partial class EntityNotificationAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Logunov.Facts/Logunov.Facts.Web/Data/Notification.cs
@@ -0,0 +1,13 @@
+﻿using Calabonga.EntityFrameworkCore.Entities.Base;
+
+namespace Logunov.Facts.Web.Data
+{
+    public class Notification:Auditable
+    {
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public bool IsCompleted { get; set; }
+        public string AddressFrom { get; set; }
+        public string AddressTo { get; set; }
+    }
+}
